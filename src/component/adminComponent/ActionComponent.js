3c5a4d5a783f4373fdf1ccd8f4becf48fbd72e7c
M ismoyo setyonowidagdo <ismoyorplb@example.com>
2020-08-02 14:45:08 +0700

    add feature crud

--- src/component/adminComponent/ActionComponent.js
+++ src/component/adminComponent/ActionComponent.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {
   Alert,
   ImageBackground,
@@ -7,16 +7,42 @@ import {
   Image,
   Text,
   TextInput,
+  TouchableOpacity,
 } from 'react-native';
+import DropDownPicker from 'react-native-dropdown-picker';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import {genreGet} from '../../redux/actions/genre';
+import {connect} from 'react-redux';
+import {BASE_URL} from '@env';
+let ActionComponent = (props) => {
+  console.log(props);
+  useEffect(() => {
+    getGenre();
+  }, []);
 
-let ActionComponent = () => {
+  let getGenre = () => {
+    let data = {
+      ConUrl: BASE_URL,
+      Search: '',
+    };
+    props.genreGet(data);
+  };
+
+  let actionData = () => {};
   return (
-    <View>
+    <ScrollView>
       <ImageBackground
         style={{width: '100%', height: 200}}
         source={require('../../main/img/images.jpeg')}>
+        <Icon
+          style={{top: 17, left: 17}}
+          name="arrow-left"
+          size={30}
+          color="white"
+        />
         <Text
           style={{
+            top: 11,
             color: 'white',
             textAlign: 'center',
             fontWeight: 'bold',
@@ -25,7 +51,7 @@ let ActionComponent = () => {
           Add Books
         </Text>
       </ImageBackground>
-      <View style={{alignContent: 'center', alignItems: 'center'}}>
+      <View style={{alignContent: 'center', alignItems: 'center', height: 700}}>
         <View
           style={{
             backgroundColor: 'white',
@@ -49,11 +75,11 @@ let ActionComponent = () => {
               top: 90,
               width: '90%',
               height: 47,
-              borderColor: 'gray',
-              borderWidth: 1,
+              borderColor: 'grey',
+              borderWidth: 3,
               borderRadius: 10,
             }}
-            placeholder="title books"
+            placeholder="     title books"
           />
           <TextInput
             multiline={true}
@@ -62,16 +88,83 @@ let ActionComponent = () => {
               top: 100,
               width: '90%',
               height: 107,
-              borderColor: 'gray',
-              borderWidth: 1,
-              borderRadius: 20,
+              borderColor: 'grey',
+              borderWidth: 3,
+              borderRadius: 12,
             }}
-            placeholder="title books"
+            placeholder="    title books"
           />
+
+          <DropDownPicker
+            style={{top: 113, width: 370, borderColor: 'grey', borderWidth: 3}}
+            items={[
+              {name_user: 'France', value: 'fr'},
+              {label: 'Spain', value: 'es'},
+              {label: 'Spain', value: 'es'},
+              {label: 'Spain', value: 'es'},
+            ]}
+            defaultNull
+            placeholder="Select genre"
+            containerStyle={{height: 40}}
+          />
+
+          <DropDownPicker
+            style={{top: 123, width: 370, borderColor: 'grey', borderWidth: 3}}
+            items={[
+              {label: 'France', value: 'fr'},
+              {label: 'Spain', value: 'es'},
+            ]}
+            defaultNull
+            placeholder="Select genre"
+            containerStyle={{height: 40}}
+            onChangeItem={(item) => this.changeCountry(item)}
+          />
+          <View
+            style={{
+              borderWidth: 3,
+              width: 370,
+              height: 50,
+              top: 140,
+              borderWidth: 3,
+              borderColor: 'gray',
+              borderRadius: 7,
+            }}>
+            <Icon
+              style={{textAlign: 'center', top: 13}}
+              name="upload"
+              size={20}
+              color="gray"
+            />
+          </View>
+          <TouchableOpacity onPress={() => actionData()}>
+            <ImageBackground
+              imageStyle={{borderRadius: 10}}
+              source={require('../../main/img/images.jpeg')}
+              style={{
+                top: 160,
+                height: 40,
+                width: 370,
+              }}>
+              <Text
+                style={{
+                  textAlign: 'center',
+                  color: 'white',
+                  top: 9,
+                  fontWeight: 'bold',
+                }}>
+                Save
+              </Text>
+            </ImageBackground>
+          </TouchableOpacity>
         </View>
       </View>
-    </View>
+    </ScrollView>
   );
 };
 
-export default ActionComponent;
+const mapStateToProps = (state) => ({
+  dataGenre: state.genreGet,
+});
+const mapDispatchToProp = {genreGet};
+
+export default connect(mapStateToProps, mapDispatchToProp)(ActionComponent);
